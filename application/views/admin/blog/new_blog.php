7ee7adc8d38dd1e3b5df53a391ec707e72d9013a
Navneet Singh <navneetofficial25@gmail.com>
2020-06-23 16:18:55 +0530

    blog pages forward

--- application/views/admin/blog/new_blog.php
+++ application/views/admin/blog/new_blog.php
@@ -2,18 +2,32 @@
 <main>
    <div class="container-fluid">
       <div class="container py-5">
+      <form action="">
          <div class="row">
             <div class="col-md-10 border-right-1">
                <div class="form-group">
                <label for="">Heading</label>
-               <input type="text" class="form-control" name="" id="" placeholder="Heading">
+               <input type="text" class="form-control" name="" id="" placeholder="Heading" required>
                </div>
-               <textarea name="about" id="textareaContent"></textarea>
+               <textarea name="about" id="textareaContent" required></textarea>
             </div>
             <div class="col-md-2">
-
+               <div class="form-group">
+                 <label for="">Categories</label>
+                 <select class="form-control form-control-sm" name="" id="">
+                   <option>Technology</option>
+                   <option>Web Development</option>
+                   <option>App Development</option>
+                 </select>
+               </div>
+               <div class="form-group">
+                 <label for="">Meta Tags</label>
+                 <textarea class="form-control" name="" id="" rows="3"></textarea>
+               </div>
+               <button type="submit" class="btn btn-success m-2 ali">Publish</button>
             </div>
        </div>
+       </form>
       </div>
    </div>
 </main>
